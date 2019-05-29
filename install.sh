e97255e051f13df5ecb85eddb98501b0f7dfa3ec
jcronq <jcronq@gmail.com>
2019-05-28 21:01:07 -0600

    better install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,19 @@
 #/bin/bash
 
-cp -R vim ~/.vim
-cp vimrc ~/.vimrc
+echo "Override your ~/.vim directory? (y/n)"
+read override_vimdir
+if [[ "$override_vimdir" == "y" ]]; then
+    cp -R vim ~/.vim
+fi
+
+echo "Override your ~/.vimrc file? (y/n)"
+read override_vimrc
+if [[ "$override_vimrc" == "y" ]]; then
+    cp vimrc ~/.vimrc
+fi
+
+echo "Override your ~/.profile file? (y/n)"
+read override_profile
+if [[ "$override_vimrc" == "y" ]]; then
+    cp profile ~/.profile
+fi
